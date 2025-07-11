8f01784dce584f5849bc8175e236cc001da994f0
Keshav-Code-er <keshavinsc446@gmail.com>
2025-07-12 00:24:59 +0530

    trigger_pipeline

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OKKK',
+        app: 'OK2',
         mongo: mongoConnected
     };
     res.json(stat);
